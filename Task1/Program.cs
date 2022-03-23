c7a32c431a31ced24135cebd7c9c35d0e834eec9
Davit Movsisyan <davitmovsisyan44@gmail.com>
2022-03-23 19:12:50 +0400

    sas

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,55 +1,55 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//namespace Task1
-//{
-//    class MyClass
-//    {
-//        public dynamic[] array = {1221,"SASA",'s' };
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Task1
+{
+    class MyClass
+    {
+        public dynamic[] array = { 1221, "SASA", 's' };
 
-//        public void AddElement(dynamic dynm)
-//        {
-//            int length2 = array.Length + 1;
-//            dynamic[] array2 = new dynamic[length2];
-//            for (int i = 0; i < array.Length; i++)
-//            {
-//                array2[i] = array[i];
-//            }
-//            array2[length2-1] = dynm;
+        public void AddElement(dynamic dynm)
+        {
 
-//        }
+            dynamic[] array2 = new dynamic[array.Length+1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array[i];
+            }
+            array2[array.Length] = dynm;
 
-//        public dynamic GetByIndex(int i)
-//        {
-//            return array[i];
-//        }
-//        public dynamic Remove(dynamic[] arr, dynamic dynm)
-//        {
+        }
 
-//            return null;
-//        }
+        public dynamic GetByIndex(int i)
+        {
+            return array[i];
+        }
+        public dynamic Remove(dynamic[] arr, dynamic dynm)
+        {
 
-//        public int GetLength()
-//        {
-//            return array.Length;
-//        }
-        
+            return null;
+        }
 
-//    }
-//     class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            
-//         MyClass myClass = new MyClass();
-//            myClass.AddElement('1');
-//            foreach (var item in myClass.array)
-//            {
-//                Console.WriteLine(item);
-//            }
-//            Console.ReadLine();
-//        }
-//    }
-//}
+        public int GetLength()
+        {
+            return array.Length;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass myClass = new MyClass();
+            myClass.AddElement('1');
+            foreach (var item in myClass.array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}

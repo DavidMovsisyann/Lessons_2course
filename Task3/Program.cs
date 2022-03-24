f17403cb7fcff4da5ee1ab7895447a243d2c589b
Davit Movsisyan <davitmovsisyan44@gmail.com>
2022-03-24 23:15:11 +0400

    adding task

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class MyClass<T>
+    {
+     T[] arr = new T[0];   
+        
+
+        public void Add(T obj)
+        {
+            T[] array2 = new T[arr.Length+ 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                array2[i] = arr[i];
+            }
+            array2[arr.Length] = obj;
+        }
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           MyClass<string> myClass = new MyClass<string>();
+            myClass.Add("Hello");
+        }
+    }
+}

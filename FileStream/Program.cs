d140745933c1d32b3b2105b85ca7a8085361c7c8
Davit Movsisyan <davitmovsisyan44@gmail.com>
2022-03-31 23:52:43 +0400

    Files

---
+++ FileStream/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace StreamWriters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\Desktop\test.txt";
+            StreamWriter streamWriter = new StreamWriter(path);
+            streamWriter.WriteLine("hello");
+            streamWriter.Close();
+        }
+    }
+}
---
+++ Files/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Uset\Desktop\test.txt";
+            if (File.Exists(path))
+            {
+                File.WriteAllText(path,"Hello");
+            }
+            
+
+        }
+    }
+}

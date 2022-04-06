ef276e3bcb5385522cf965d4a068a9a2cdd82ae7
Davit Movsisyan <davitmovsisyan44@gmail.com>
2022-04-06 23:45:29 +0400

    .

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RandMet();
+        }
+
+        public static void RandMet()
+        {
+          Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+            Console.WriteLine(random.Next(1,100));
+            Thread.Sleep(5000);
+
+            }
+        }
+    }
+}
